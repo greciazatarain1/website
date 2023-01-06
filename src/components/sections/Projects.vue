f4ab45e0b56040edee594e92eb03672db7f188bd
Grecia <greciazatarain@gmail.com>
2023-01-06 15:55:03 +0100

    add new project

--- src/components/sections/Projects.vue
+++ src/components/sections/Projects.vue
@@ -19,7 +19,7 @@
                         </ul>
                         <div class="links">
                             <a :href="project.githubUrl" target="_blank">Check code</a>
-                            <a :href="project.liveUrl" target="_blank">See online</a>
+                            <a :href="project.liveUrl" target="_blank">View online</a>
                         </div>
                     </div>
                 </li>
@@ -37,10 +37,18 @@ export default {
                 {
                     name: "Restaurant website",
                     description: "A website for a restaurant with Menu, About and Contact pages.",
-                    tech: ["Vue", "HTML", "SCSS"],
+                    tech: ["Vue.js", "HTML", "SCSS"],
                     image: require("@/assets/img-project-01.jpg"),
                     githubUrl: "https://github.com/greciazatarain1/restaurant-website",
                     liveUrl: "https://greciazatarain1.github.io/project-restaurant/"
+                },
+                {
+                    name: "Recipe app",
+                    description: "App to search for recipes.",
+                    tech: ["HTML", "JavaScript", "CSS"],
+                    image: require("@/assets/img-project-02.jpg"),
+                    githubUrl: "https://github.com/greciazatarain1/recipe-app",
+                    liveUrl: "https://greciazatarain1.github.io/recipe-app/"
                 }
             ]
         }
@@ -55,30 +63,37 @@ section {
     width: 100%;
     overflow: hidden;
     &::before {
+        display: none;
         content: "";
         position: absolute;
         bottom: 198px;
-        right: -561px;
+        left: -561px;
         width: 800px;
         height: 800px;
         border: 3px solid #91937c;
-        display: block;
+        display: none;
         border-radius: 50%;
+
+        @include desktop {
+            left: -430px;
+            display: block;
+        }
     }
     &::after {
         content: "";
         position: absolute;
         bottom: -431px;
-        right: -709px;
+        left: -709px;
         width: 800px;
         height: 800px;
         border: 3px solid #8d6e85;
-        display: block;
+        display: none;
         border-radius: 50%;
-        border-left: 10px solid #8d6e85;
+        border-right: 10px solid #8d6e85;
 
         @include desktop {
-            right: -561px;
+            left: -430px;
+            display: block;
         }
     }
 }
@@ -86,13 +101,31 @@ section {
     display: flex;
     position: relative;
     z-index: 1;
+    flex-direction: column;
 
     .item-project {
         justify-content: space-between;
         flex-direction: column;
+        padding: 30px 0;
 
         @include desktop {
+            padding: 50px 0;
             flex-direction: row;
+        }
+
+        &:nth-child(even){
+
+            @include desktop {
+            flex-direction: row-reverse;
+
+                .text-container {
+                    text-align: right;
+                }
+
+                .tech {
+                    justify-content: flex-end;
+                }
+            }
         }
     }
 
@@ -105,6 +138,8 @@ section {
     }
 
     .text-container {
+        width: 100%;
+
         @include desktop {
             width: 62%;
         }
@@ -141,6 +176,10 @@ section {
 
             &:hover {
                 border-color: #78508f;
+            }
+
+            &:last-of-type{
+                margin-right: 0;
             }
         }
     }
